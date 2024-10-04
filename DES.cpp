1784d2b6c4581b33428042c3c0f64e66b5f2195f
Leon Magbanua <112917169+MagbanLeon@users.noreply.github.com>
2024-10-04 17:09:21 +0000

    start

--- DES.cpp
+++ DES.cpp
@@ -1,1 +1,11 @@
 #include<iostream>
+using namespace std;
+
+ int main(int argc, char* argv[]){
+    if(argc != 3){
+        cout << "There aren't enough parameters!" << endl << "Enter as './des sample_input.txt output.txt'" << endl;
+        return -1;
+    }
+    
+    return 0;
+ }
