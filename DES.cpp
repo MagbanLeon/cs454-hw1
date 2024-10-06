9688449184a7775f637753b3cbcc4df8a50c1575
Leon Magbanua <112917169+MagbanLeon@users.noreply.github.com>
2024-10-06 20:44:34 +0000

    function

--- DES.cpp
+++ DES.cpp
@@ -10,7 +10,7 @@ using namespace std;
 //Function Declarations
 unsigned long int decryption();
 unsigned long int encryption();
-void keys(unsigned long int initialKey);
+void keys(unsigned long int initialKey, unsigned long int cArray[16], unsigned long int dArray[16], unsigned long int keyArray[16]);
 
 //Main Function
 int main(int argc, char* argv[]){
@@ -82,7 +82,7 @@ unsigned long int encryption(){
 
 }
 
-void keys(unsigned long int initialKey){
+void keys(unsigned long int initialKey, unsigned long int cArray[16], unsigned long int dArray[16], unsigned long int keyArray[16]){
     int pc1[56] = {57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,
     44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4};
     unsigned long int keyPlus = 0;
@@ -116,4 +116,7 @@ void keys(unsigned long int initialKey){
             d = d & ~ (1 << i);
         }
     }
+    int leftShiftSched[16] = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
+    //getting Cn and Dn
+
 }   
