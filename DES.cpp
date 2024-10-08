7ff6c209600febfce0bdd3f959736cec3820f2f7
Leon Magbanua <112917169+MagbanLeon@users.noreply.github.com>
2024-10-08 03:23:16 +0000

    sure yeah whatever

--- DES.cpp
+++ DES.cpp
@@ -148,7 +148,7 @@ unsigned long int decryption(unsigned long int keyArray[16], unsigned long int e
     return original;
 }
 void decryptLeftRight(unsigned long int left[17], unsigned long int right[17], unsigned long int keys[16]){
-    for(int i = 15; i > -1; i--){
+    for(int i = 15; i > 0; i--){
         //left[i] = right[i-1];
         right[i] = left[i+1];
         //right[i] = left[i-1] ^ functionF(right[i-1], keys[i]);
@@ -174,8 +174,238 @@ unsigned long int undoF(unsigned long int finalResult, unsigned long int key){
     }
 
 
+    int s1[4][16] = {{14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7},
+			        {0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8},
+			        {4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
+			        {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13}};
 
+    int s2[4][16] = {{15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10},
+                    {3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5},
+                    {0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15},
+                    {13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9}};
 
+    int s3[4][16] = {{10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8},
+                    {13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1},
+                    {13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7},
+                    {1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12}};
+
+    int s4[4][16] = {{ 7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15},
+                    {13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9},
+                    {10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4},
+                    {3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14}};
+
+    int s5[4][16] = {{ 2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9},
+                    {14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6},
+                    {4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14},
+                    {11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3}};
+
+    int s6[4][16] = {{12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11},
+                    {10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8},
+                    {9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6},
+                    {4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13}};
+
+    int s7[4][16] = {{ 4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1},
+                    {13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6},
+                    {1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2},
+                    {6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12}};
+
+    int s8[4][16] = {{13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7},
+                    {1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2},
+                    {7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8},
+                    {2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11}};
+    //========================================================
+    //s8
+    row = 0, column = 0;
+    do{
+        row++;
+        do{
+            column++;
+        }while(sResult[7] != s8[row][column]);
+    }while(sResult[7] != s8[row][column]);
+
+        //getting row
+    bit = (row & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 5UL << 5;
+    bit = (row & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 0UL << 0;
+        //getting column
+    bit = (column & ( 1UL << 3 )) >> 3;
+    if(bit == 1) eRxorK = eRxorK | 4UL << 4;
+    bit = (column & ( 1UL << 2 )) >> 2;
+    if(bit == 1) eRxorK = eRxorK | 3UL << 3;
+    bit = (column & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 2UL << 2;
+    bit = (column & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 1UL << 1;
+
+    //s7
+    row = 0, column = 0;
+    do{
+        row++;
+        do{
+            column++;
+        }while(sResult[7] != s7[row][column]);
+    }while(sResult[6] != s7[row][column]);
+
+        //getting row
+    bit = (row & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 11UL << 11;
+    bit = (row & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 6UL << 6;
+        //getting column
+    bit = (column & ( 1UL << 3 )) >> 3;
+    if(bit == 1) eRxorK = eRxorK | 10UL << 10;
+    bit = (column & ( 1UL << 2 )) >> 2;
+    if(bit == 1) eRxorK = eRxorK | 9UL << 9;
+    bit = (column & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 8UL << 8;
+    bit = (column & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 7UL << 7;
+
+    //s6
+    row = 0, column = 0;
+    do{
+        row++;
+        do{
+            column++;
+        }while(sResult[5] != s6[row][column]);
+    }while(sResult[5] != s6[row][column]);
+
+        //getting row
+    bit = (row & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 17UL << 17;
+    bit = (row & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 12UL << 12;
+        //getting column
+    bit = (column & ( 1UL << 3 )) >> 3;
+    if(bit == 1) eRxorK = eRxorK | 16UL << 16;
+    bit = (column & ( 1UL << 2 )) >> 2;
+    if(bit == 1) eRxorK = eRxorK | 15UL << 15;
+    bit = (column & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 14UL << 14;
+    bit = (column & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 13UL << 13;
+    
+    //s5
+    row = 0, column = 0;
+    do{
+        row++;
+        do{
+            column++;
+        }while(sResult[4] != s5[row][column]);
+    }while(sResult[4] != s5[row][column]);
+
+        //getting row
+    bit = (row & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 23UL << 23;
+    bit = (row & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 18UL << 18;
+        //getting column
+    bit = (column & ( 1UL << 3 )) >> 3;
+    if(bit == 1) eRxorK = eRxorK | 22UL << 22;
+    bit = (column & ( 1UL << 2 )) >> 2;
+    if(bit == 1) eRxorK = eRxorK | 21UL << 21;
+    bit = (column & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 20UL << 20;
+    bit = (column & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 19UL << 19;
+    
+    //s4
+    row = 0, column = 0;
+    do{
+        row++;
+        do{
+            column++;
+        }while(sResult[3] != s4[row][column]);
+    }while(sResult[3] != s4[row][column]);
+
+        //getting row
+    bit = (row & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 29UL << 29;
+    bit = (row & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 24UL << 24;
+        //getting column
+    bit = (column & ( 1UL << 3 )) >> 3;
+    if(bit == 1) eRxorK = eRxorK | 28UL << 28;
+    bit = (column & ( 1UL << 2 )) >> 2;
+    if(bit == 1) eRxorK = eRxorK | 27UL << 27;
+    bit = (column & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 26UL << 26;
+    bit = (column & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 25UL << 25;
+    
+    //s3
+    row = 0, column = 0;
+    do{
+        row++;
+        do{
+            column++;
+        }while(sResult[2] != s3[row][column]);
+    }while(sResult[2] != s3[row][column]);
+
+        //getting row
+    bit = (row & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 35UL << 35;
+    bit = (row & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 30UL << 30;
+        //getting column
+    bit = (column & ( 1UL << 3 )) >> 3;
+    if(bit == 1) eRxorK = eRxorK | 34UL << 34;
+    bit = (column & ( 1UL << 2 )) >> 2;
+    if(bit == 1) eRxorK = eRxorK | 33UL << 33;
+    bit = (column & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 32UL << 32;
+    bit = (column & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 31UL << 31;
+    
+    //s2
+    row = 0, column = 0;
+    do{
+        row++;
+        do{
+            column++;
+        }while(sResult[1] != s2[row][column]);
+    }while(sResult[1] != s2[row][column]);
+
+        //getting row
+    bit = (row & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 41UL << 41;
+    bit = (row & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 36UL << 36;
+        //getting column
+    bit = (column & ( 1UL << 3 )) >> 3;
+    if(bit == 1) eRxorK = eRxorK | 40UL << 40;
+    bit = (column & ( 1UL << 2 )) >> 2;
+    if(bit == 1) eRxorK = eRxorK | 39UL << 39;
+    bit = (column & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 38UL << 38;
+    bit = (column & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 37UL << 37;
+    
+    //s1
+    row = 0, column = 0;
+    do{
+        row++;
+        do{
+            column++;
+        }while(sResult[0] != s1[row][column]);
+    }while(sResult[0] != s1[row][column]);
+
+        //getting row
+    bit = (row & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 47UL << 47;
+    bit = (row & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 42UL << 42;
+        //getting column
+    bit = (column & ( 1UL << 3 )) >> 3;
+    if(bit == 1) eRxorK = eRxorK | 46UL << 46;
+    bit = (column & ( 1UL << 2 )) >> 2;
+    if(bit == 1) eRxorK = eRxorK | 45UL << 45;
+    bit = (column & ( 1UL << 1 )) >> 1;
+    if(bit == 1) eRxorK = eRxorK | 44UL << 44;
+    bit = (column & ( 1UL << 0 )) >> 0;
+    if(bit == 1) eRxorK = eRxorK | 43UL << 43;
+    //========================================================
     eR = eRxorK ^ key;
 
     int eTable[] =  {32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,
